d1c633f4a30bbfb6846b2aee09019cdd16fee093
Frank <fuzan@fuzandeMacBook-Pro.local>
2019-07-05 23:06:22 +0800

    add axios for api calling

---
+++ src/api/config.js
@@ -0,0 +1,10 @@
+// api 超时时间
+export const timeout = 10000;
+// api base url
+export const baseURL = "/";
+// 跨域请求是否携带 cookie
+export const enableCookieWhenCross = true;
+// 默认请求头
+export const requestHeader = {
+  'Content-Type': 'application/x-www-form-urlencoded',
+};
