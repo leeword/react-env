94f457c2891c03a0b2ed93c85c7bfdf3fdcc87c9
Frank <fuzan@fuzandeMacBook-Pro.local>
2019-07-20 11:43:41 +0800

    change webpack config

--- build/webpack.dev.js
+++ build/webpack.dev.js
@@ -1,122 +1,68 @@
 const path = require('path');
 const webpack = require('webpack');
+const merge = require('webpack-merge');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const HappyPack = require('happypack');
-const FriendErrorsWebpackPlugin = require('friendly-errors-webpack-plugin');
+const baseConfig = require('./webpack.base');
 
-const happyThreadPool = HappyPack.ThreadPool({ size: 5 });
-
-module.exports = {
-    // Enables NamedChunksPlugin and NamedModulesPlugin.
-    mode: 'development',
-    entry: [
-        'react-hot-loader/patch',
-        './src/main.js',
-    ],
-    output: {
-        filename: '[name].js',
-        path: path.resolve(__dirname, '../dist'),
-        chunkFilename: '[name].chunk.js',
-        publicPath: '/',
-        pathinfo: false,
+module.exports = merge(baseConfig, {
+  // Enables NamedChunksPlugin and NamedModulesPlugin.
+  mode: 'development',
+  entry: [
+    'react-hot-loader/patch',
+    './src/main.js',
+  ],
+  output: {
+    filename: '[name].js',
+    path: path.resolve(__dirname, '../dist'),
+    chunkFilename: '[name].chunk.js',
+    publicPath: '/',
+    pathinfo: false,
+  },
+  resolve: {
+    alias: {
+      'react-dom$': '@hot-loader/react-dom',
     },
-    resolve: {
-        alias: {
-            react: path.resolve(__dirname, '../node_modules/react/index.js'),
-            'react-dom': '@hot-loader/react-dom',
-        },
-        mainFields: ['module', 'main'],
-        extensions: ['.js', '.json'],
-        modules: [path.resolve(__dirname, '../node_modules')]
-    },
-    module: {
-        rules: [
-            {
-                parser: {
-                    requireEnsure: false,
-                }
-            },
-            {
-                test: /\.m?js$/,
-                include: path.resolve(__dirname, '../src'),
-                use: {
-                    loader: 'happypack/loader?id=babel',
-                },
-            },
-            {
-                test: /\.(sa|sc|c)ss$/,
-                use: {
-                    loader: 'happypack/loader?id=styles',
-                },
-            },
-            {
-                test: /\.(gif|png|jpe?g|webp|svg)$/i,
-                use: [
-                    {
-                        loader: 'url-loader',
-                    },
-                ],
-            },
-            {
-                test: /\.(woff2?|eot|ttf|otf)$/,
-                use: {
-                    loader: 'file-loader',
-                    options: {
-                        name: '[name].[ext]',
-                    },
-                },
-            },
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(gif|png|jpe?g|webp|svg)$/i,
+        use: [
+          {
+            loader: 'url-loader',
+          },
         ],
-    },
-    devtool: 'cheap-module-eval-source-map',
-    devServer: {
-        hot: true,
-        open: true,
-        historyApiFallback: true,
-        watchOptions: {
-            ignored: /node_modules/,
-            aggregateTimeout: 300,
-            poll: 1000,
-        },
-    },
-    plugins: [
-        new webpack.DllReferencePlugin({
-            manifest: require('../dll/react.manifest.json'),
-        }),
-        new HtmlWebpackPlugin({
-            title: "react 模版",
-            template: path.resolve(__dirname, '../public/index-dev.html'),
-        }),
-        new HappyPack({
-            id: 'babel',
-            threadPool: happyThreadPool,
-            loaders: [
-                'babel-loader?cacheDirectory',
-                'eslint-loader',
-            ],
-        }),
-        new HappyPack({
-            id: 'styles',
-            threadPool: happyThreadPool,
-            loaders: [
-                {
-                    loader: 'style-loader'
-                },
-                {
-                    loader: "css-loader",
-                    options: {
-                        importLoaders: 1,
-                    },
-                },
-                "sass-loader",
-            ],
-        }),
-        new FriendErrorsWebpackPlugin(),
+      },
     ],
-    optimization: {
-        removeAvailableModules: false,
-        removeEmptyChunks: false,
-        splitChunks: false,
+  },
+  devtool: 'cheap-module-eval-source-map',
+  devServer: {
+    hot: true,
+    open: true,
+    historyApiFallback: true,
+    overlay: {
+      errors: true,
     },
     stats: 'errors-only',
-}
+    watchOptions: {
+      ignored: /node_modules/,
+      aggregateTimeout: 300,
+      poll: 1000,
+    },
+  },
+  plugins: [
+    // require dll manifest
+    new webpack.DllReferencePlugin({
+      manifest: require('../dll/react.manifest.json'),
+    }),
+    new HtmlWebpackPlugin({
+      title: 'react 模版',
+      template: path.resolve(__dirname, '../public/index-dev.html'),
+    }),
+  ],
+  optimization: {
+    removeAvailableModules: false,
+    removeEmptyChunks: false,
+    splitChunks: false,
+  },
+})
--- build/webpack.prod.js
+++ build/webpack.prod.js
@@ -1,210 +1,153 @@
 const path = require('path');
 const glob = require('glob');
 const webpack = require('webpack');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const OptimizeCssAssetsWebpackPlugin = require('optimize-css-assets-webpack-plugin');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const merge = require('webpack-merge');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-const HappyPack = require('happypack');
-const LodashModuleReplacementPlugin = require('lodash-webpack-plugin');
-const PurifyCSSPlugin = require('purifycss-webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const InlineManifestWebpackPlugin = require('inline-manifest-webpack-plugin');
 const FixStyleOnlyEntriesPlugin = require('webpack-fix-style-only-entries');
-const FriendErrorsWebpackPlugin = require('friendly-errors-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const OptimizeCssAssetsWebpackPlugin = require('optimize-css-assets-webpack-plugin');
+const PurifyCSSPlugin = require('purifycss-webpack');
+const baseConfig = require('./webpack.base');
 
-const happyThreadPool = HappyPack.ThreadPool({ size: 5 });
-// TODO: css 关键路径渲染 https://github.com/addyosmani/critical-path-css-tools
-// TODO: srcset 控制浏览器加载图片类型 resize-image-loader responsive-loader
-const config = {
-    mode: 'production',
-    entry: {
-        main: './src/main.js',
-    },
-    output: {
-        // using contenthash after webpack 4.17.0 for javascript
-        filename: 'js/[name].[contenthash:8].js',
-        path: path.resolve(__dirname, '../dist'),
-        chunkFilename: 'js/[name].[contenthash:8].js',
-        publicPath: '/',
-    },
-    resolve: {
-        alias: {
-            react: path.resolve(__dirname, '../node_modules/react/index.js'),
-        },
-        mainFields: ['module', 'main'],
-        extensions: ['.js', '.json'],
-        modules: [path.resolve(__dirname, '../node_modules')]
-    },
-    module: {
-        rules: [
-            {
-                parser: {
-                    // disabled require.ensure in dynamic import case
-                    requireEnsure: false,
-                }
+const config = merge(baseConfig, {
+  mode: 'production',
+  entry: {
+    main: './src/main.js',
+  },
+  output: {
+    // use contenthash after webpack 4.17.0 for javascript long term caching
+    filename: 'js/[name].[contenthash:8].js',
+    path: path.resolve(__dirname, '../dist'),
+    chunkFilename: 'js/[name].[contenthash:8].js',
+    publicPath: '/',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(gif|png|jpe?g|webp|svg)$/i,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 1024 * 10,
+              name: 'image/[name].[hash:8].[ext]',
+              fallback: 'file-loader',
             },
-            {
-                test: /\.m?js$/,
-                // 
-                include: path.resolve(__dirname, '../src'),
-                use: {
-                    loader: 'happypack/loader?id=babel',
-                },
+          },
+          {
+            loader: 'image-webpack-loader',
+            options: {
+              mozjpeg: {
+                progressive: true,
+                quality: 65,
+              },
+              optipng: {
+                enabled: false,
+              },
+              pngquant: {
+                quality: '65-90',
+                speed: 4,
+              },
+              gifsicle: {
+                interlaced: false,
+              },
+              webp: {
+                quality: 75,
+              },
             },
-            {
-                test: /\.(sa|sc|c)ss$/,
-                use: [
-                    MiniCssExtractPlugin.loader,
-                    {
-                        loader: 'happypack/loader?id=styles',
-                    },
-                ],
-            },
-            {
-                test: /\.(gif|png|jpe?g|webp|svg)$/i,
-                use: [
-                    {
-                        loader: 'url-loader',
-                        options: {
-                            limit: 1024 * 10,
-                            name: 'image/[name].[hash:8].[ext]',
-                            fallback: 'file-loader',
-                        },
-                    },
-                    {
-                        loader: 'image-webpack-loader',
-                        options: {
-                            mozjpeg: {
-                                progressive: true,
-                                quality: 65
-                            },
-                            optipng: {
-                                enabled: false,
-                            },
-                            pngquant: {
-                                quality: '65-90',
-                                speed: 4
-                            },
-                            gifsicle: {
-                                interlaced: false,
-                            },
-                            webp: {
-                                quality: 75
-                            }
-                        },
-                    },
-                ],
-            },
-            {
-                test: /\.(woff2?|eot|ttf|otf)$/,
-                use: {
-                    loader: 'file-loader',
-                    options: {
-                        name: 'font/[name].[hash:8].[ext]',
-                    },
-                },
-            },
+          },
         ],
-    },
-    plugins: [
-        new CleanWebpackPlugin(),
-        // give dynamic chunk a name instead of id when `webpackChunkName` is not defined
-        // if we use auto-increment id by default, and then we hardly to have a stable file signature
-        new webpack.NamedChunksPlugin(
-            chunk => chunk.name || Array.from(chunk.modulesIterable, m => path.relative(m.context, m.request)).join("_")
-        ),
-        // help split lodash
-        new LodashModuleReplacementPlugin(),
-        new HtmlWebpackPlugin({
-            title: "react 模版",
-            template: path.resolve(__dirname, '../public/index-prod.html'),
-            // optimize html template
-            minify: {
-                removeComments: true,
-                collapseWhitespace: true,
-                removeRedundantAttributes: true,
-                useShortDoctype: true,
-                removeEmptyAttributes: true,
-                removeStyleLinkTypeAttributes: true,
-                minifyJS: true,
-                minifyCSS: true,
-                minifyURLs: true,
-            },
-        }),
-        // inject webpack runtime code into html file
-        new InlineManifestWebpackPlugin('manifest'),
-        new HappyPack({
-            id: 'styles',
-            threadPool: happyThreadPool,
-            loaders: [
-                {
-                    loader: 'css-loader',
-                    options: {
-                        importLoaders: 2,
-                    },
-                },
-                'postcss-loader',
-                'sass-loader',
-            ],
-        }),
-        // fix css generate extra js file in webpack4
-        new FixStyleOnlyEntriesPlugin(),
-        new MiniCssExtractPlugin({
-            filename: 'style/[name].[contenthash:8].css',
-            chunkFileName: 'style/[name].[contenthash:8].chunk.css',
-        }),
-        // minify css
-        new OptimizeCssAssetsWebpackPlugin(),
-        // remove unused selector from CSS file
-        // see https://github.com/webpack-contrib/purifycss-webpack
-        new PurifyCSSPlugin({
-            paths: glob.sync(`${path.join(__dirname, "../src")}/**/*.js`, { nodir: true }),
-        }),
-        new HappyPack({
-            id: 'babel',
-            threadPool: happyThreadPool,
-            loaders: ['babel-loader?cacheDirectory'],
-        }),
-        // make the output more clean and friendly in the terminal interface
-        new FriendErrorsWebpackPlugin(),
+      },
     ],
-    optimization: {
-        moduleIds: 'hashed',
-        splitChunks: {
-            cacheGroups: {
-                vendors: {
-                    test: /[\\/]node_modules[\\/]/,
-                    name: 'vendors',
-                    chunks: 'all',
-                    priority: -10,
-                    minChunks: 1,
-                },
-                commons: {
-                    name: 'commons',
-                    chunks: 'initial',
-                    minChunks: 2,
-                    priority: -9,
-                },
-                styles: {
-                    name: 'styles',
-                    test: /\.css$/,
-                    chunks: 'all',
-                    enforce: true,
-                },
-            },
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    // give dynamic chunk a name instead of id when `webpackChunkName` is not defined
+    // if we use auto-increment id by default, and then we hardly to have a stable file signature
+    new webpack.NamedChunksPlugin(
+      chunk => chunk.name || Array.from(chunk.modulesIterable, m => path.relative(m.context, m.request)).join('_'),
+    ),
+    new HtmlWebpackPlugin({
+      title: 'react 模版',
+      template: path.resolve(__dirname, '../public/index-prod.html'),
+      // optimize html template
+      minify: {
+        removeComments: true,
+        collapseWhitespace: true,
+        removeRedundantAttributes: true,
+        useShortDoctype: true,
+        removeEmptyAttributes: true,
+        removeStyleLinkTypeAttributes: true,
+        minifyJS: true,
+        minifyCSS: true,
+        minifyURLs: true,
+      },
+    }),
+    // inject webpack runtime code into html file
+    new InlineManifestWebpackPlugin('manifest'),
+    // fix css generate extra js file in webpack4
+    new FixStyleOnlyEntriesPlugin(),
+    new MiniCssExtractPlugin({
+      filename: 'style/[name].[contenthash:8].css',
+      chunkFileName: 'style/[name].[contenthash:8].chunk.css',
+    }),
+    // minify css
+    new OptimizeCssAssetsWebpackPlugin(),
+    // remove unused selector from CSS file
+    // see https://github.com/webpack-contrib/purifycss-webpack
+    new PurifyCSSPlugin({
+      paths: glob.sync(path.resolve(__dirname, '../src/**/*.js'), { nodir: true }),
+    }),
+  ],
+  optimization: {
+    moduleIds: 'hashed',
+    splitChunks: {
+      cacheGroups: {
+        react: {
+          test: /[\\/]node_modules[\\/](react|react-dom)[\\/]/,
+          name: 'react',
+          chunks: 'all',
+          priority: 100,
         },
-        runtimeChunk: {
-            name: "manifest",
+        varied: {
+          test: /[\\/]node_modules[\\/](lodash|core-js)[\\/]\S*\.js/,
+          name: 'varied',
+          chunks: 'all',
+          priority: 80,
         },
+        vendors: {
+          test: /[\\/]node_modules[\\/]/,
+          name: 'vendors',
+          chunks: 'all',
+          priority: 60,
+        },
+        commons: {
+          name: 'commons',
+          chunks: 'initial',
+          minChunks: 2,
+          priority: 20,
+        },
+        // styles: {
+        //   name: 'styles',
+        //   test: /\.css$/,
+        //   chunks: 'all',
+        //   enforce: true,
+        // },
+      },
     },
-    stats: 'errors-only',
-    performance: false,
-}
+    runtimeChunk: {
+      name: 'manifest',
+    },
+  },
+  performance: false,
+})
 
 // analysis bundle size
 if (process.env.SHOW_REPORT === '1') {
-    const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
-    config.plugins.push(new BundleAnalyzerPlugin())
+  const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
+  config.plugins.push(new BundleAnalyzerPlugin())
 }
 
 module.exports = config;
