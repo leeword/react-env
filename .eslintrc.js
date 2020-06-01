354fd39c941345877c896b2387536409d3e504d6
Frank <fuzan@fuzandeMacBook-Pro.local>
2020-06-02 00:45:12 +0800

    fix(lint): repair eslint error

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,50 +2,50 @@ const OFF = 0;
 const ERROR = 2;
 
 module.exports = {
-    root: true,
-    extends: ['airbnb'],
-    parser: 'babel-eslint',
-    parserOptions: {
-        ecmaVersion: 10,
-        sourceType: 'module',
-    },
-    plugins: [
-        'react',
-    ],
-    env: {
-        browser: true,
-        node: true,
-    },
-    rules: {
-        'semi': OFF,
-        'global-require': OFF,
-        'import/prefer-default-export': 'off',
-        'no-param-reassign': 'off',
-        'no-unused-expressions': ERROR,
-        'no-unused-vars': [ERROR, { args: 'none' }],
-        'no-use-before-define': [ERROR, { functions: false, variables: false }],
-        'import/no-extraneous-dependencies': [
-            'error',
-            {
-                'devDependencies': [
-                    'build/**',
-                    'test/**',
-                ],
-                'optionalDependencies': false,
-                'peerDependencies': false,
-            }
+  root: true,
+  extends: ['airbnb'],
+  parser: 'babel-eslint',
+  parserOptions: {
+    ecmaVersion: 10,
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+  ],
+  env: {
+    browser: true,
+    node: true,
+  },
+  rules: {
+    'semi': OFF,
+    'global-require': OFF,
+    'import/prefer-default-export': 'off',
+    'no-param-reassign': 'off',
+    'no-unused-expressions': ERROR,
+    'no-unused-vars': [ERROR, { args: 'none' }],
+    'no-use-before-define': [ERROR, { functions: false, variables: false }],
+    'import/no-extraneous-dependencies': [
+      'error',
+      {
+        'devDependencies': [
+          'build/**',
+          'test/**',
         ],
-        'no-useless-concat': OFF,
-        'no-var': ERROR,
+        'optionalDependencies': false,
+        'peerDependencies': false,
+      }
+    ],
+    'no-useless-concat': OFF,
+    'no-var': ERROR,
 
-        'jsx-quotes': [ERROR, 'prefer-double'],
-        'space-before-blocks': ERROR,
-        'space-before-function-paren': OFF,
-        'quotes': [ERROR, 'single', { avoidEscape: true, allowTemplateLiterals: true }],
+    'jsx-quotes': [ERROR, 'prefer-double'],
+    'space-before-blocks': ERROR,
+    'space-before-function-paren': OFF,
+    'quotes': [ERROR, 'single', { avoidEscape: true, allowTemplateLiterals: true }],
 
-        'react/jsx-indent': [ERROR, 2],
-        'react/jsx-filename-extension': [OFF, {
-            extensions: ['.js', '.jsx'],
-        }]
-    },
+    'react/jsx-indent': [ERROR, 2],
+    'react/jsx-filename-extension': [OFF, {
+      extensions: ['.js', '.jsx'],
+    }]
+  },
 }
