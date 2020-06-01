5e292924c1fb148eee2441b0d68ff43506076f85
leeword <fz1986856560@gmail.com>
2020-06-02 00:48:28 +0800

    fix(lint): repair eslint error (#16)

--- src/sw-register.js
+++ src/sw-register.js
@@ -1,17 +1,15 @@
-/* eslint-disable */
+/* eslint-disable no-console */
 function isHttps() {
   return window.location.protocol === 'https:';
 }
 
 function isLocalhost() {
   return Boolean(
-    window.location.hostname === 'localhost' ||
+    window.location.hostname === 'localhost'
     // [::1] is the IPv6 localhost address.
-    window.location.hostname === '[::1]' ||
+    || window.location.hostname === '[::1]'
     // 127.0.0.1/8 is considered localhost for IPv4.
-    window.location.hostname.match(
-      /^127(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$/
-    )
+    || window.location.hostname.match(/^127(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$/),
   );
 }
 
@@ -21,7 +19,7 @@ function registerValidSW(swUrl) {
     reg.onupdatefound = () => {
       const installingWorker = reg.installing
 
-      installingWorker.onstatechange = function () {
+      installingWorker.onstatechange = () => {
         switch (installingWorker.state) {
           case 'installed':
             if (navigator.serviceWorker.controller) {
@@ -36,6 +34,8 @@ function registerValidSW(swUrl) {
             break;
           case 'redundant':
             console.error('The installing service worker became redundant.');
+            break;
+          default:
             break;
         }
       }
